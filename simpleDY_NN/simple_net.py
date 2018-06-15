394f44a76d90e788a02fb2d44e2aaa88273b9047
tmitchel <tyler106@ksu.edu>
2018-06-14 23:06:09 -0500

    add simple fully connected network to start

--- simpleDY_NN/simple_net.py
+++ simpleDY_NN/simple_net.py
@@ -3,6 +3,50 @@
 from os import environ
 environ['KERAS_BACKEND'] = 'tensorflow'
 
-from keras.models import Sequential, Model
-from keras.layers import Input, Activation, Dense, Convolution2D, MaxPooling2D, Dropout, Flatten
+import os
+import h5py
+from keras.models import Model
+from keras.layers import Input, Activation, Dense
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="Run neural network to separate Z->TT + 2 jets from VBF")
+parser.add_argument('--verbose', action='store_true',
+                    dest='verbose', default=False,
+                    help='run in verbose mode'
+                    )
+parser.add_argument('--nhid', '-n', action='store',
+                    dest='nhid', default=5, type=int,
+                    help='number of hidden nodes in network'
+                    )
+parser.add_argument('--rebuild', action='store_true',
+                    dest='rebuild', default=False,
+                    help='rebuild the nn model'
+                    )
+args = parser.parse_args()
+
+def build_nn(nhid):
+  input_length = 7
+  inputs = Input(shape = (input_length,), name = 'input')
+  hidden = Dense(nhid, name = 'hidden', kernel_initializer = 'normal', activation = 'sigmoid')(inputs)
+  outputs = Dense(2, name = 'output', kernel_initializer = 'normal', activation = 'sigmoid')(hidden)
+  model = Model(inputs = inputs, outputs = outputs)
+
+  model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+  if args.verbose:
+    model.summary()
+
+    # early stopping callback
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+
+    # model checkpoint callback
+    model_checkpoint = ModelCheckpoint('simple.hdf5', monitor='val_loss',
+    verbose=0, save_best_only=True,
+    save_weights_only=False, mode='auto',
+    period=1)
+
+  return model
+
+if __name__ == "__main__":
+  model = build_nn(args.nhid)
