008eb3641d4db4fd8609cd2774262440f5cdd4c1
tmitchel <tyler106@ksu.edu>
2018-06-20 16:05:57 -0500

    add to setup.sh

--- simpleDY_NN/setup.sh
+++ simpleDY_NN/setup.sh
@@ -6,6 +6,9 @@ fi
 source pyenv/bin/activate # activate.csh for tcsh
 eval `scramv1 runtime -sh` # cmsenv
 
+pip install h5py
+pip install matplotlib
+pip install -U scikit-learn
 pip install keras
 pip install tables
 pip install pandas
