18f425ad1318c1245897a8185aa2ba2dc6516d83
Tyler Mitchell <tyler106@ksu.edu>
2018-09-27 10:55:10 -0500

    cleaning

--- train_network.py
+++ train_network.py
@@ -16,10 +16,6 @@ parser.add_argument('--vars', '-v', nargs='+', action='store',
                     dest='vars', default=['Q2V1', 'Q2V2'],
                     help='variables to input to network'
                     )
-parser.add_argument('--njet', '-N', action='store_true',
-                    dest='njet', default=False,
-                    help='run on DY + n-jets'
-                    )
 parser.add_argument('--model_name', '-m', action='store',
                     dest='model_name', default=None,
                     help='name of a trained model'
@@ -32,7 +28,6 @@ parser.add_argument('--dont_save_json', '-d', action='store_true',
 args = parser.parse_args()
 n_user_inputs = 0
 
-import h5py
 import pandas
 import numpy as np
 from glob import glob
@@ -41,8 +36,69 @@ from pprint import pprint
 from root_pandas import read_root
 environ['KERAS_BACKEND'] = 'tensorflow'  ## on Wisc machine, must be before Keras import
 from keras.models import Model
-from keras.layers import Input, Activation, Dense
+from keras.layers import Input, Dense
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+##############################
+## Just the plotting things ##
+##############################
+def build_plots(history, label_test, other=None):
+  """ do whatever plotting is needed """
+  import matplotlib.pyplot as plt
+  from sklearn.metrics import roc_curve, auc
+
+  plt.figure(figsize=(15, 10))
+
+  # Plot ROC
+  label_predict = model.predict(data_test)
+  fpr, tpr, thresholds = roc_curve(label_test[:, 0], label_predict[:, 0])
+  roc_auc = auc(fpr, tpr)
+  plt.plot([0, 1], [0, 1], linestyle='--', lw=2,
+           color='k', label='random chance')
+  plt.plot(tpr, fpr, lw=2, color='cyan', label='NN auc = %.3f' % (roc_auc))
+  if other != None:
+    fpr2, tpr2, thresholds2, roc_auc2 = other
+    plt.plot(tpr2, fpr2, lw=2, color='red',
+             label='MELA auc = %.3f' % (roc_auc2))
+  plt.xlim([0, 1.0])
+  plt.ylim([0, 1.0])
+  plt.xlabel('true positive rate')
+  plt.ylabel('false positive rate')
+  plt.title('receiver operating curve')
+  plt.legend(loc="upper left")
+  plt.savefig('plots/'+args.model_name+'.pdf')
+
+  # plot loss vs epoch
+  ax = plt.subplot(2, 1, 1)
+  ax.plot(history.history['loss'], label='loss')
+  ax.plot(history.history['val_loss'], label='val_loss')
+  ax.legend(loc="upper right")
+  ax.set_xlabel('epoch')
+  ax.set_ylabel('loss')
+
+  # plot accuracy vs epoch
+  ax = plt.subplot(2, 1, 2)
+  ax.plot(history.history['acc'], label='acc')
+  ax.plot(history.history['val_acc'], label='val_acc')
+  ax.legend(loc="upper left")
+  ax.set_xlabel('epoch')
+  ax.set_ylabel('acc')
+  plt.show()
+
+def MELA_ROC(sig, bkg):
+  """ read h5 file and return info for making a ROC curve from MELA disc. """
+  from sklearn.metrics import roc_curve, auc
+  all_data = pandas.concat([sig, bkg])
+  dataset = all_data.values
+  data = dataset[:, 0:1]  # read Dbkg_VBF for all events
+  labels = dataset[:, -1]  # read labels for all events
+  fpr, tpr, thresholds = roc_curve(labels, data[:, 0])
+  roc_auc = auc(fpr, tpr)  # calculate Area Under Curve
+  return fpr, tpr, thresholds, roc_auc
+
+##############################
+## End the plotting things  ##
+##############################
 
 def create_json(model_name):
   import json
@@ -58,10 +114,9 @@ def create_json(model_name):
         'model_name': model_name,
         'variables': args.vars,
         'nhidden': args.nhid,
-        'njet': args.njet,
-        'n_user_inputs': int(n_user_inputs)
+        'n_user_inputs': int(n_user_inputs) 
       }, fout
-    )
+    ) 
 
 def build_nn(nhid):
   """ Build and return the model with callback functions """
@@ -78,10 +133,7 @@ def build_nn(nhid):
   if args.model_name != None:
     model_name = args.model_name + '.hdf5'
   else:
-    if args.njet:
-      model_name = 'NN_njet_model.hdf5'
-    else:
-      model_name = 'NN_2jet_model.hdf5'
+    model_name = 'NN_model.hdf5'
 
   model_name = 'models/' + model_name
 
@@ -102,23 +154,16 @@ def massage_data(vars, fname, sample_type):
   from ROOT import TLorentzVector
 
   #print 'Slicing and dicing...', fname.split('.root')[0].split('input_files/')[-1]
-  other_vars = ['evtwt', 'passSelection', 'Dbkg_VBF']
+  other_vars = ['evtwt', 'cat_vbf', 'Dbkg_VBF']
   slicer = vars + other_vars  ## add variables for event selection
   
-  ## Switch to using root_pandas
-  df = read_root(fname, columns=slicer) 
-  df_roc = pandas.DataFrame()
-  df_roc['Dbkg_VBF'] = df[(df['passSelection'] > 0) & (df['Dbkg_VBF'] > 0)]['Dbkg_VBF']
+  df = read_root(fname, columns=slicer) ## read only necessary columns
+  df_roc = pandas.DataFrame() ## empty dataframe to hold Dbkg_VBF for ROC curve
+  df_roc['Dbkg_VBF'] = df[(df['cat_vbf'] > 0) & (df['Dbkg_VBF'] > 0)]['Dbkg_VBF'] ## get Dbkg_VBF when reasonable and passes selection
   
   print 'Input data is now in the DataFrame'
 
-  ## additional variables for selection that must be constructed can be added here
-
-  ## define additional event selection
-
-  print 'Begin the slicing'
-
-  qual_cut = (df['passSelection'] > 0) & (df['Q2V1'] > 0)
+  qual_cut = (df['cat_vbf'] > 0) & (df['Q2V1'] > 0) ## make sure event passes selection and has reasonable values
   df = df[qual_cut]
 
   if 'bkg' in sample_type:
@@ -136,70 +181,14 @@ def massage_data(vars, fname, sample_type):
 
     ## format bkg DataFrame for MELA ROC curve
     df_roc['isSignal'] = np.ones(len(df_roc))
-    
-  ## additional input variables that must be constructed can be added here
-  #df = AddInput(db, 'dEtajj', abs(df['jeta_1'] - df['jeta_2']))
 
+  ## make sure the weight is in the correct column
   weight = df['evtwt']
   df.insert(loc=df.shape[1], column='weight', value=weight)
   
   ## drop event selection branches from NN input
   df = df.drop(other_vars, axis=1)
   return df, df_roc
-
-
-def AddInput(dataf, name, val):
-   dataf.insert(loc=dataf.shape[1], column=name, value=val)
-   global n_user_inputs
-   n_user_inputs += .5
-   return dataf
-
-def build_plots(history, label_test, other=None):
-  """ do whatever plotting is needed """
-  import  matplotlib.pyplot  as plt
-  from sklearn.metrics import roc_curve, auc
-
-  plt.figure(figsize=(15,10))
-
-  # Plot ROC
-  label_predict = model.predict(data_test)
-  fpr, tpr, thresholds = roc_curve(label_test[:,0], label_predict[:,0])
-  roc_auc = auc(fpr, tpr)
-  plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='k', label='random chance')
-  plt.plot(tpr, fpr, lw=2, color='cyan', label='NN auc = %.3f' % (roc_auc))
-  if other != None:
-    fpr2, tpr2, thresholds2, roc_auc2 = other
-    plt.plot(tpr2, fpr2, lw=2, color='red', label='MELA auc = %.3f' % (roc_auc2))
-  plt.xlim([0, 1.0])
-  plt.ylim([0, 1.0])
-  plt.xlabel('true positive rate')
-  plt.ylabel('false positive rate')
-  plt.title('receiver operating curve')
-  plt.legend(loc="upper left")
-  plt.savefig('plots/'+args.model_name+'.pdf')
-  #plt.show()
-  # if args.njet:
-  #   ext = 'njet'
-  #   plt.savefig('plots/layer1_node{}_njet_NN.pdf'.format(args.nhid))
-  # else:
-  #   plt.savefig('plots/layer1_node{}_2jet_NN.pdf'.format(args.nhid))
-
-  # plot loss vs epoch
-  ax = plt.subplot(2, 1, 1)
-  ax.plot(history.history['loss'], label='loss')
-  ax.plot(history.history['val_loss'], label='val_loss')
-  ax.legend(loc="upper right")
-  ax.set_xlabel('epoch')
-  ax.set_ylabel('loss')
-
-  # plot accuracy vs epoch
-  ax = plt.subplot(2, 1, 2)
-  ax.plot(history.history['acc'], label='acc')
-  ax.plot(history.history['val_acc'], label='val_acc')
-  ax.legend(loc="upper left")
-  ax.set_xlabel('epoch')
-  ax.set_ylabel('acc')
-  plt.show()
 
 def final_formatting(data, labels):
   """ split data into testing and validation then scale and return collections """
@@ -216,17 +205,6 @@ def final_formatting(data, labels):
 
   ## return the same that train_test_split does, but normalized
   return data_train_val, data_test, label_train_val, label_test
-
-def MELA_ROC(sig, bkg):
-  """ read h5 file and return info for making a ROC curve from MELA disc. """
-  from sklearn.metrics import roc_curve, auc
-  all_data = pandas.concat([sig, bkg])
-  dataset = all_data.values
-  data = dataset[:,0:1]  ## read Dbkg_VBF for all events
-  labels = dataset[:,-1]  ## read labels for all events
-  fpr, tpr, thresholds = roc_curve(labels, data[:,0])
-  roc_auc = auc(fpr, tpr)  ## calculate Area Under Curve
-  return fpr, tpr, thresholds, roc_auc
 
 if __name__ == "__main__":
   ## format the data
@@ -258,7 +236,7 @@ if __name__ == "__main__":
                     verbose=args.verbose, # switch to 1 for more verbosity
                     callbacks=callbacks,
                     validation_split=0.25,
-                    #sample_weight=weights
+                    sample_weight=weights
   )
 
   if args.verbose:
@@ -270,9 +248,7 @@ if __name__ == "__main__":
   else:
     if args.model_name != None:
       model_name = args.model_name
-    elif args.njet:
-      model_name = 'NN_njet_model'
     else:
-      model_name = 'NN_2jet_model'
+      model_name = 'NN_model'
 
     create_json(model_name)
